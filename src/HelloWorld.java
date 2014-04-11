f970c278c83425135836910764dc23169d100b63
antreascig <Antreas-cig@hotmail.com>
2014-04-11 14:13:08 +0100

    Example Working

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+// HelloWorld.java
+public class HelloWorld 
+{
+	
+	public static void main(String[] args)
+	{
+		say();
+	}
+	
+    public static void say() {
+        System.out.println("Hello");
+    }
+   
+}
